ecff7b9d58065c82dd9c031eac23af476f84f578
VoDinhNghia <vodinhnghia85@gmail.com>
2023-04-12 16:34:33 +0700

    move search into menu bar

--- src/components/homePage/components/leftPage/leftPage.jsx
+++ src/components/homePage/components/leftPage/leftPage.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Button from "react-bootstrap/Button";
-import { BsSearch } from "react-icons/bs";
 import { connect } from "react-redux";
-import { postAction } from "../../../../store/action";
 import "./index.css";
 
 class LeftHomePage extends Component {
@@ -15,43 +10,9 @@ class LeftHomePage extends Component {
     };
   }
 
-  onChangeSearch(event) {
-    const key = event.target.value;
-    this.setState({
-      searchKey: key,
-    });
-    if (key?.length > 2) {
-        this.searchPost()
-    }
-  }
-
-  searchPost() {
-    const { dispatch } = this.props;
-    const { searchKey } = this.state;
-    setTimeout(() => {
-      dispatch({ type: postAction.GET_ALL_POST, payload: { searchKey } });
-    }, 100);
-  }
-
   render() {
     return (
       <>
-        <InputGroup className="SearchLeftHomePage">
-          <Form.Control
-            placeholder="search post by title..."
-            aria-label="search post"
-            aria-describedby="basic-addon-search-home-page"
-            onChange={(event) => this.onChangeSearch(event)}
-            className="InputSearchHomePage"
-          />
-          <Button
-            id="basic-addon-search-home-page"
-            variant="light"
-            onClick={() => this.searchPost()}
-          >
-            <BsSearch />
-          </Button>
-        </InputGroup>
         <div className="leftMenu">
           <h4>left menu home page</h4>
           <p>some design such as search, user list online...</p>
