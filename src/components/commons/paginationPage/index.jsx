3c538eace4cd92058ad6e657d9c09b70849e4747
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-22 09:30:15 +0700

    update UI study space

--- src/components/commons/paginationPage/index.jsx
+++ src/components/commons/paginationPage/index.jsx
@@ -7,27 +7,31 @@ class PaginationPage extends Component {
     const { page = 0, totalPage = 0 } = this.props;
 
     return (
-      <Card className="mb-3 text-center border-0">
-        <Card.Body>
-          <Button
-            variant="outline-primary"
-            onClick={() => this.props.goToBackPage()}
-            size="sm"
-          >
-            <BsChevronDoubleLeft />
-          </Button>{" "}
-          <span>
-            Tổng số: {totalPage}, Trang hiện tại: {page}{" "}
-          </span>
-          <Button
-            variant="outline-primary"
-            onClick={() => this.props.goToNextPage(totalPage)}
-            size="sm"
-          >
-            <BsChevronDoubleRight />
-          </Button>
-        </Card.Body>
-      </Card>
+      <>
+        {totalPage > 0 ? (
+          <Card className="mb-3 text-center border-0">
+            <Card.Body>
+              <Button
+                variant="outline-primary"
+                onClick={() => this.props.goToBackPage()}
+                size="sm"
+              >
+                <BsChevronDoubleLeft />
+              </Button>{" "}
+              <span>
+                Tổng số: {totalPage}, Trang hiện tại: {page}{" "}
+              </span>
+              <Button
+                variant="outline-primary"
+                onClick={() => this.props.goToNextPage(totalPage)}
+                size="sm"
+              >
+                <BsChevronDoubleRight />
+              </Button>
+            </Card.Body>
+          </Card>
+        ) : null}
+      </>
     );
   }
 }
