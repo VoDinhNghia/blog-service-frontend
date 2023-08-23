90ec472a074545891ce6f337f1257b8e6b2301b6
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-23 09:05:24 +0700

    write UI message page

--- src/components/messagePage/leftPage/index.jsx
+++ src/components/messagePage/leftPage/index.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
 
 class MessageLeftPage extends Component {
   render() {
     return (
-      <div>
-        <p>left page</p>
+      <div className="bg-light p-1 mb-2">
+        <p className="m-3">
+          <img src="/image/icon-login.png" className="IconUserMessage" alt="" />
+          <Link>Vo Dinh Nghia</Link>
+        </p>
+        <p className="m-3">
+          <img src="/image/icon-login.png" className="IconUserMessage" alt="" />
+          <Link>Nguyen Hoang Bao Han</Link>
+        </p>
       </div>
     );
   }
--- src/utils/messageHandle.js
+++ src/utils/messageHandle.js
@@ -1,11 +1,16 @@
 import AuthenService from "../services/authService";
 
 export const getNumberMsgNotRead = (messages = []) => {
+  const listMessages = getMessageNotRead(messages);
+  return listMessages?.length;
+};
+
+export const getMessageNotRead = (messages = []) => {
   const currentUser = AuthenService.getCurrentUser();
   const listMessages = messages?.filter((conver) =>
     conver?.messages?.find(
       (msg) => msg?.userSendId !== currentUser?.id && !msg?.status
     )
   );
-  return listMessages?.length;
+  return listMessages;
 };
