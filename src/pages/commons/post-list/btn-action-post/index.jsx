802bc66d84703e3877fe2121bb7e27773a8b2d0b
VoDinhNghia <vodinhnghia85@gmail.com>
2024-06-08 20:22:36 +0700

    [Refactor Code] refactor post list component

---
+++ src/pages/commons/post-list/btn-action-post/index.jsx
@@ -0,0 +1,92 @@
+import { Component } from "react";
+import { OverlayTrigger, Popover, Button } from "react-bootstrap";
+import {
+  BsFillReplyFill,
+  BsFillHandThumbsUpFill,
+  BsFillEmojiHeartEyesFill,
+  BsFillChatLeftTextFill,
+} from "react-icons/bs";
+import { AiFillHeart } from "react-icons/ai";
+import { likeAction } from "../../../../constants/constant";
+import { displayBtnLikePost, findCurrentUserAction } from "../../../../utils/home.util";
+
+class DisplayBtnActionPost extends Component {
+  render() {
+    const {
+      post = {},
+      currentUser = {},
+      openedCommentId = "",
+      actionLike,
+      showComment,
+      actionShare,
+    } = this.props;
+    const isLike = findCurrentUserAction(post, currentUser, likeAction.LIKE);
+    const isHeart = findCurrentUserAction(post, currentUser, likeAction.HEART);
+    const isLove = findCurrentUserAction(post, currentUser, likeAction.LOVE);
+    const objBtnLike = displayBtnLikePost(isLike, isHeart, isLove);
+
+    return (
+      <p className="LikeShareComment text-center">
+        <OverlayTrigger
+          trigger="click"
+          placement="top"
+          overlay={
+            <Popover>
+              <Button
+                variant="outline-light"
+                onClick={() => actionLike(post?.id, likeAction.LIKE)}
+                style={{ color: `${isLike ? "blue" : "gray"}`, fontSize: 25 }}
+              >
+                <BsFillHandThumbsUpFill />
+              </Button>
+              <Button
+                variant="outline-light"
+                onClick={() => actionLike(post?.id, likeAction.HEART)}
+                style={{ color: `${isHeart ? "red" : "gray"}`, fontSize: 25 }}
+              >
+                <AiFillHeart />
+              </Button>
+              <Button
+                variant="outline-light"
+                onClick={() => actionLike(post?.id, likeAction.LOVE)}
+                style={{
+                  color: `${isLove ? "#e810c0" : "gray"}`,
+                  fontSize: 25,
+                }}
+              >
+                <BsFillEmojiHeartEyesFill />
+              </Button>
+            </Popover>
+          }
+        >
+          <Button
+            className="ButtonLSC"
+            variant="outline-light"
+            style={{ color: objBtnLike.color }}
+          >
+            {objBtnLike.icon} {objBtnLike.title}
+          </Button>
+        </OverlayTrigger>
+        <Button
+          className="ButtonLSC"
+          variant="outline-light"
+          aria-expanded={openedCommentId === post?.id}
+          onClick={() => showComment(post?.id)}
+        >
+          <BsFillChatLeftTextFill /> Bình luận
+        </Button>{" "}
+        <Button
+          className="ButtonLSC"
+          variant="outline-light"
+          onClick={() => actionShare(post?.id)}
+          disabled={currentUser?.id === post?.user?.id ? true : false}
+          style={currentUser?.id === post?.user?.id ? { color: "gray" } : {}}
+        >
+          <BsFillReplyFill /> Chia sẻ
+        </Button>
+      </p>
+    );
+  }
+}
+
+export default DisplayBtnActionPost;
--- src/utils/home.util.js
+++ src/utils/home.util.js
@@ -1,5 +1,46 @@
+import {
+  BsFillHandThumbsUpFill,
+  BsFillEmojiHeartEyesFill,
+} from "react-icons/bs";
+import { AiFillHeart } from "react-icons/ai";
+
 export const filterStatusLoginUser = (userList = [], searchKey) => {
   return userList?.filter((user) =>
     searchKey ? user?.statusLogin || !user?.statusLogin : user?.statusLogin
   );
+};
+
+export const countEmotionPost = (actions = [], type = "") => {
+  const count = actions.filter((p) => p?.action === type)?.length;
+  return count;
+};
+
+export const findCurrentUserAction = (post = {}, currentUser, type) => {
+  return post?.likes?.find(
+    (p) => p?.user?.id === currentUser?.id && p?.action === type
+  );
+};
+
+export const displayBtnLikePost = (isLike, isHeart, isLove) => {
+  let color = "gray";
+  let title = "Thích";
+  let icon = <BsFillHandThumbsUpFill />;
+  if (isLike) {
+    color = "blue";
+  }
+  if (isHeart) {
+    color = "#e810c0";
+    title = "Thả tim";
+    icon = <AiFillHeart />;
+  }
+  if (isLove) {
+    color = "red";
+    title = "Yêu thích";
+    icon = <BsFillEmojiHeartEyesFill />;
+  }
+  return {
+    color,
+    title,
+    icon,
+  };
 };
