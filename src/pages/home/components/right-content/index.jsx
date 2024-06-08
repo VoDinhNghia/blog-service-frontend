928c0c9bccae718b3dd9318908cfcc2fd6f04c70
VoDinhNghia <vodinhnghia85@gmail.com>
2024-06-08 09:21:48 +0700

    [Refactor Code] refactor pagination action home page

--- src/pages/home/components/right-content/index.jsx
+++ src/pages/home/components/right-content/index.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import PostListHomePage from "../../../commons/post-list";
 import NewPostCommon from "../../../commons/create-post";
 import PaginationPage from "../../../commons/pagination";
+import { calCurrentPage, calToTalPage } from "../../../../utils/util";
 
 class RightHomePage extends Component {
   constructor(props) {
@@ -27,25 +28,9 @@ class RightHomePage extends Component {
     this.dispatch({ type: postAction.GET_ALL_POST, payload: { page, limit } });
   }
 
-  goToNextPage(totalPage) {
-    const currentPage =
-      this.state.page < totalPage ? this.state.page + 1 : totalPage;
-    this.setState({
-      page: currentPage,
-    });
-    setTimeout(() => {
-      this.dispatch({
-        type: postAction.GET_ALL_POST,
-        payload: {
-          page: currentPage,
-          limit: this.state.limit,
-        },
-      });
-    }, 100);
-  }
-
-  goToBackPage() {
-    const currentPage = this.state.page > 1 ? this.state.page - 1 : 1;
+  goToPage(totalPage = 0, isNextPage = true) {
+    const { page } = this.state;
+    const currentPage = calCurrentPage(page, totalPage, isNextPage);
     this.setState({
       page: currentPage,
     });
@@ -59,7 +44,7 @@ class RightHomePage extends Component {
 
   render() {
     const { postLists = [], total = 0 } = this.props;
-    const totalPage = Math.round(Number(total / this.state.limit) + 0.45);
+    const totalPage = calToTalPage(total, this.state.limit);
 
     return (
       <>
@@ -72,8 +57,8 @@ class RightHomePage extends Component {
         <PaginationPage
           page={this.state.page}
           totalPage={totalPage}
-          goToBackPage={() => this.goToBackPage()}
-          goToNextPage={() => this.goToNextPage(totalPage)}
+          goToBackPage={() => this.goToPage(0, false)}
+          goToNextPage={() => this.goToPage(totalPage, true)}
         />
       </>
     );
